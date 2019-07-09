3cd0432fbcc6870b60889af7d64da28b3247f907
Goks <firehawk005@gmail.com>
2019-07-09 17:07:31 +0530

    Initial Functions

--- src/App.vue
+++ src/App.vue
@@ -53,13 +53,15 @@
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+                                    v-model="sendMail"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+                                    v-model="sendMail"> Send Infomail
                         </label>
                     </div>
 
@@ -71,13 +73,15 @@
                         <input
                                 type="radio"
                                 id="male"
-                                value="Male"> Male
+                                value="Male"
+                                v-model="gender"> Male
                     </label>
                     <label for="female">
                         <input
                                 type="radio"
                                 id="female"
-                                value="Female"> Female
+                                value="Female"
+                                v-model="gender"> Female
                     </label>
                 </div>
             </div>
@@ -86,22 +90,28 @@
                     <label for="priority">Priority</label>
                     <select
                             id="priority"
-                            class="form-control">
-                        <option></option>
+                            class="form-control"
+                            v-model="selectedPriority">
+                        <option v-for="priority in priorities">{{priority}}</option>
                     </select>
                 </div>
+            </div>
+            <div class="row">
+              <app-Switch v-model="dataSwitch"></app-Switch>
             </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                            @click.prevent="submitted"
+                            >Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="this.isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -114,11 +124,11 @@
                         <p style="white-space: pre;">Message: {{  message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="item in sendMail">{{item}}</li>
                         </ul>
-                        <p>Gender:</p>
-                        <p>Priority:</p>
-                        <p>Switched:</p>
+                        <p>Gender: {{gender}}</p>
+                        <p>Priority: {{selectedPriority}}</p>
+                        <p>Switched: {{dataSwitch}}</p>
                     </div>
                 </div>
             </div>
@@ -127,6 +137,9 @@
 </template>
 
 <script>
+
+import Switch from './components/Switch';
+
     export default {
         data:function(){
             return  {
@@ -135,8 +148,22 @@
                   Password:'',
                   Age:27
                 },
-                message:  'Your Complaints Here!'
+                message:  'Your Complaints Here!',
+                sendMail:[],
+                gender: 'Male',
+                selectedPriority: '',
+                priorities: ['High', 'Medium', 'Low'],
+                dataSwitch: true,
+                isSubmitted: false
             }
+        },
+        components:{
+          appSwitch: Switch
+        },
+        methods:{
+          submitted: function(){
+            this.isSubmitted=true;
+          }
         }
     }
 </script>
