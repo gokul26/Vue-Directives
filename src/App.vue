05da4dac106c42d7b3f831dcef084fe7838e02a8
Goks <firehawk005@gmail.com>
2019-07-09 17:08:21 +0530

    Exercise Done

--- src/App.vue
+++ src/App.vue
@@ -1,164 +1,124 @@
 <template>
     <div class="container">
         <form>
-            <div class="row">
+            <div class="row" v-if="!isSubmitted">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <h1>File a Complaint</h1>
-                    <hr>
+                    <!-- Exercise 1 -->
+                    <h1>Sign UP</h1>
+                    <!-- Create a Signup Form where you retrieve the following Information -->
+                    <!-- Full Name (First Name + Last Name) -->
+                    <!-- <div class="form-group">
+                        <label for="email">First Name</label>
+                        <input
+                                type="text"
+                                class="form-control"
+                                v-model="userexData.fname"
+                            >
+                    </div>
+                    <div class="form-group">
+                        <label for="lname">Last Name</label>
+                        <input
+                                type="text"
+                                class="form-control"
+                                v-model="userexData.lname"
+                            >
+                    </div> -->
+                    
+                    <app-Fullname v-model="fullname"></app-Fullname>
+                    <!-- Mail -->
                     <div class="form-group">
                         <label for="email">Mail</label>
                         <input
                                 type="text"
-                                id="email"
                                 class="form-control"
-                                v-model="userData.email"
+                                v-model="userexData.mail"
                             >
                     </div>
+                    <!-- Password -->
                     <div class="form-group">
                         <label for="password">Password</label>
                         <input
                                 type="password"
-                                id="password"
                                 class="form-control"
-                                v-model.lazy="userData.Password"
+                                v-model.lazy="userexData.password"
                                 >
-                                {{ userData.Password }}
                     </div>
+                    <!-- Store Data? Yes/No -->
                     <div class="form-group">
-                        <label for="age">Age</label>
-                        <input
-                                type="number"
-                                id="age"
-                                class="form-control"
-                                v-model="userData.Age">
-                    </div>
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="message">Message</label><br>
-                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-                    <textarea
-                            id="message"
-                            rows="5"
-                            class="form-control"
-                            v-model="message"></textarea>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <div class="form-group">
-                        <label for="sendmail">
+                        Store Data?
+                        <label for="male">
                             <input
-                                    type="checkbox"
-                                    id="sendmail"
-                                    value="SendMail"
-                                    v-model="sendMail"> Send Mail
+                                    type="radio"
+                                    value="Yes"
+                                    v-model="dataStore"> Yes
                         </label>
-                        <label for="sendInfomail">
+                        <label for="female">
                             <input
-                                    type="checkbox"
-                                    id="sendInfomail"
-                                    value="SendInfoMail"
-                                    v-model="sendMail"> Send Infomail
+                                    type="radio"
+                                    value="No"
+                                    v-model="dataStore"> No
                         </label>
                     </div>
 
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="male">
-                        <input
-                                type="radio"
-                                id="male"
-                                value="Male"
-                                v-model="gender"> Male
-                    </label>
-                    <label for="female">
-                        <input
-                                type="radio"
-                                id="female"
-                                value="Female"
-                                v-model="gender"> Female
-                    </label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
-                    <label for="priority">Priority</label>
-                    <select
-                            id="priority"
-                            class="form-control"
-                            v-model="selectedPriority">
-                        <option v-for="priority in priorities">{{priority}}</option>
-                    </select>
-                </div>
-            </div>
-            <div class="row">
-              <app-Switch v-model="dataSwitch"></app-Switch>
-            </div>
-            <hr>
-            <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
                             class="btn btn-primary"
                             @click.prevent="submitted"
-                            >Submit!
+                            >Add Info
                     </button>
                 </div>
             </div>
-        </form>
-        <hr>
-        <div class="row" v-if="this.isSubmitted">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Your Data</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Mail:{{ userData.email }}</p>
-                        <p>Password: {{ userData.Password }}</p>
-                        <p>Age: {{ userData.Age }}</p>
-                        <p style="white-space: pre;">Message: {{  message }}</p>
-                        <p><strong>Send Mail?</strong></p>
-                        <ul>
-                            <li v-for="item in sendMail">{{item}}</li>
-                        </ul>
-                        <p>Gender: {{gender}}</p>
-                        <p>Priority: {{selectedPriority}}</p>
-                        <p>Switched: {{dataSwitch}}</p>
+            <!-- Exercise 2 -->
+            <!-- Only display the Form if it has NOT been submitted -->
+            <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+            <div class="row" v-else>
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4>Your Data</h4>
+                        </div>
+                        <div class="panel-body">
+                            <p>Full Name: {{ fullname }}</p>
+                            <p>Mail: {{ userexData.mail }}</p>
+                            <p>Password: {{ userexData.password }}</p>
+                            <p><strong>Store data?</strong> {{dataStore}}</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div class="row">
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
 
-import Switch from './components/Switch';
+// import Switch from './components/Switch';
+import Fullname from './components/Fullname';
 
     export default {
         data:function(){
             return  {
-                userData:{
-                  email:'',
-                  Password:'',
-                  Age:27
+                userexData:{
+                  fname:'',
+                  lname:'',
+                  mail:'',
+                  Password:''
                 },
-                message:  'Your Complaints Here!',
-                sendMail:[],
-                gender: 'Male',
-                selectedPriority: '',
-                priorities: ['High', 'Medium', 'Low'],
                 dataSwitch: true,
-                isSubmitted: false
+                isSubmitted: false,
+                dataStore: 'Yes',
+                fullname:'Max Green',
             }
         },
         components:{
-          appSwitch: Switch
+        //   appSwitch: Switch,
+          appFullname: Fullname
         },
         methods:{
           submitted: function(){
