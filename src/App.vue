8b809661c4306a63ddd361ed10cc245733251121
Goks <firehawk005@gmail.com>
2019-07-13 10:27:20 +0530

    Local Filters

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,27 @@
     <div class="container">
         <div class="row">
             <h1>Filters and Mixins</h1>
+            <p>{{text | toUpperCase}}</p>
+            <p>{{text | toLowerCase}}</p>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    data:function () {
+        return {
+            text: 'Hellow THere'
+        }
+    },
+    filters:{
+        toUpperCase:function(val){
+            return val.toUpperCase();
+        },
+        toLowerCase:function(val){
+            return val.toLowerCase();
+        }
+    }
 }
 </script>
 
